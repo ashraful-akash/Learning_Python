af1faaea7ba20ff7263e88b35ebe983e4fafc15f
Md. Ashraful Hossen Akash <ashrafulhossainakash@gmail.com>
2026-01-02 19:04:05 +0600

    Add files via upload

---
+++ 4_madlibs_game.py
@@ -0,0 +1,14 @@
+#madlibs game
+#word game where you create a story
+#by filling in blanks with random words
+
+adjective1 = input("Enter an adjective(Description): ")
+noun1 = input("Enter an noun(person,place,thing): ")
+adjective2 = input("Enter an adjective(Description): ")
+verb1 = input("Enter a verb ending with 'ing': ")
+adjective3 = input("Enter an adjective(Description): ")
+
+print(f"Today I went to {adjective1} zoo.")
+print(f"In an exhibit , I saw {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}")
