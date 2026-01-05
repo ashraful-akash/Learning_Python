f407db9d0c090a1abbd3c993ac49e7d485a1f24a
Md. Ashraful Hossen Akash <ashrafulhossainakash@gmail.com>
2026-01-05 10:59:38 +0600

    Add files via upload

---
+++ 19_2D_collections.py
@@ -0,0 +1,30 @@
+fruits = ["apple","orange","banana","coconut"]
+veg =    ["celery","carrots","potatoes"]
+meats =  ["chicken","fish","turkey"]
+
+groceries = [fruits,veg,meats]
+
+# fruits[0]="pineapple"
+# print(fruits)
+
+#print(groceries[2])
+
+for collection in groceries:
+    for food in collection:
+        print(food, end=" ")
+    print()
+
+
+#creating num pad
+
+num_pad =((1,2,3),
+          (4,5,6),
+          (7,8,9),
+          ("*",0,"#"))
+
+print(num_pad)
+
+for row in num_pad:
+    for num in row:
+        print(num, end=" ")
+    print()
